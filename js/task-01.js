98dc6b8758e323b1f2c93c0e6c5c4bffd51ad674
IgBovt <140014814+IgBovt@users.noreply.github.com>
2023-11-22 21:41:07 +0200

    fixed task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,7 @@
 const list = document.querySelector("#categories");
 console.log(`This list has ${list.children.length} categories`);
 
-[...list.children].forEach((element) =>
-  console.log(
-    `${element.firstElementChild.textContent} has ${element.lastElementChild.children.length} elements`
-  )
-);
+[...list.children].forEach((element) => {
+  console.log(`Category: ${element.firstElementChild.textContent}`);
+  console.log(`Elements: ${element.lastElementChild.children.length}`);
+});
