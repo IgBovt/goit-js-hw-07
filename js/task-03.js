83847f92191d2eae89c14cf5bc77bce10055c52f
IgBovt <140014814+IgBovt@users.noreply.github.com>
2023-11-25 19:45:48 +0200

    task #3
    optimizated function

--- js/task-03.js
+++ js/task-03.js
@@ -1,12 +1,12 @@
-const pageInputEl = document.querySelector("#name-input");
-const outputEl = document.querySelector("#name-output");
-
-const getUserName = (input, output) => {
-  input.addEventListener("input", (event) => {
-    event.currentTarget.value === ""
-      ? (output.textContent = "Anonymous")
-      : (output.textContent = event.currentTarget.value);
-  });
+const refs = {
+  input: document.querySelector("#name-input"),
+  output: document.querySelector("#name-output"),
 };
 
-getUserName(pageInputEl, outputEl);
+const textChanger = (event) => {
+  event.currentTarget.value === ""
+    ? (refs.output.textContent = "Anonymous")
+    : (refs.output.textContent = event.currentTarget.value);
+};
+
+refs.input.addEventListener("input", textChanger);
