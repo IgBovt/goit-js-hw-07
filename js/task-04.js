db2623bce430c588227482131a2960364168a9a8
IgBovt <140014814+IgBovt@users.noreply.github.com>
2023-11-25 20:35:07 +0200

    made task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,17 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  event.target.elements.email.value === "" ||
+  event.target.elements.password.value === ""
+    ? alert("All form fields must be filled in")
+    : console.log({
+        email: event.target.elements.email.value,
+        password: event.target.elements.password.value,
+      });
+
+  event.currentTarget.reset();
+}
