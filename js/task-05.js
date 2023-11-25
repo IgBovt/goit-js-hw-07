fafd55dc573d0725dc5d98e39c0988cc2012f6ae
IgBovt <140014814+IgBovt@users.noreply.github.com>
2023-11-25 20:52:38 +0200

    made task#5

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,18 @@
+const refs = {
+  button: document.querySelector(".change-color"),
+  output: document.querySelector(".color"),
+  body: document.querySelector("body"),
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeBodyColor() {
+  refs.output.textContent = getRandomHexColor();
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+refs.button.addEventListener("click", changeBodyColor);
