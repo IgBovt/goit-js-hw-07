d37a6c93b41ace22b41d8c741828c22195d866e7
IgBovt <140014814+IgBovt@users.noreply.github.com>
2023-11-26 16:43:09 +0200

    fixed task#4

--- js/task-04.js
+++ js/task-04.js
@@ -5,13 +5,17 @@ formRef.addEventListener("submit", handleFormSubmit);
 function handleFormSubmit(event) {
   event.preventDefault();
 
-  event.target.elements.email.value === "" ||
-  event.target.elements.password.value === ""
-    ? alert("All form fields must be filled in")
-    : console.log({
-        email: event.target.elements.email.value,
-        password: event.target.elements.password.value,
-      });
+  if (
+    event.target.elements.email.value === "" ||
+    event.target.elements.password.value === ""
+  ) {
+    return alert("All form fields must be filled in");
+  } else {
+    console.log({
+      email: event.target.elements.email.value,
+      password: event.target.elements.password.value,
+    });
+  }
 
   event.currentTarget.reset();
 }
