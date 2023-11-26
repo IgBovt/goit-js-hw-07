2ea7695e4a624a78d2201b75f7fd30de1e0f10a5
IgBovt <140014814+IgBovt@users.noreply.github.com>
2023-11-26 18:29:27 +0200

    made styles a little bit

--- js/task-01.js
+++ js/task-01.js
@@ -5,3 +5,20 @@ console.log(`This list has ${listEl.children.length} categories`);
   console.log(`Category: ${element.firstElementChild.textContent}`);
   console.log(`Elements: ${element.lastElementChild.children.length}`);
 });
+
+// ====== STYLES =====//
+const smallListEl = document.querySelectorAll(".item ul");
+smallListEl.forEach((list) => list.classList.add("small-list"));
+
+const smallListItemEl = document.querySelectorAll(".small-list li");
+smallListItemEl.forEach((item) => item.classList.add("small-list-item"));
+
+// ======= FONTS ======= //;
+
+const headEL = document.querySelector("head");
+headEL.insertAdjacentHTML(
+  "beforeend",
+  `<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap" rel="stylesheet"></link>`
+);
